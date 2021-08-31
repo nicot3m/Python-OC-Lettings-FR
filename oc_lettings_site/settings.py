749f029c1264897e1abe0745b395968bc0c79875
nicot3m <nicot3m@gmail.com>
2021-08-31 18:28:51 +0200

    Commit30. Remove django_heroku in settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,8 +1,8 @@
 import os
 
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
